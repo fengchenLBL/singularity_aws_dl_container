cdecc9046430dd827431e9563b3c0d21c4223d37
fengchen <aric@tp.gateway.2wire.net>
2020-07-02 11:03:53 -0700

    added the missing bash file

---
+++ container/build_singularity_local_test_gpu.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+####################################################
+########## Download and unzip the dataset ##########
+####################################################
+cd ../data/
+wget https://danilop.s3-eu-west-1.amazonaws.com/reInvent-Workshop-Data-Backup.zip && unzip reInvent-Workshop-Data-Backup.zip
+mv reInvent-Workshop-Data-Backup/* ./
+rm -rf reInvent-Workshop-Data-Backup reInvent-Workshop-Data-Backup.zip
+cd ../container/
+
+###################################################################################
+######### Build the SageMaker Container & Convert it to Singularity image #########
+###################################################################################
+algorithm_name=sagemaker-keras-text-classification
+
+chmod +x sagemaker_keras_text_classification/train
+chmod +x sagemaker_keras_text_classification/serve
+
+# Get the region defined in the current configuration
+region=$(aws configure get region)
+fullname="local_${algorithm_name}:latest"
+
+# Get the login command from ECR and execute it directly
+$(aws ecr get-login --no-include-email --region ${region} --registry-ids 763104351884)
+
+# Build the docker image locally with the image name
+# In the "Dockerfile", modify the source image to select one of the available deep learning docker containers images:
+# https://aws.amazon.com/releasenotes/available-deep-learning-containers-images
+docker build  -t ${algorithm_name} -f Dockerfile_gpu .
+docker tag ${algorithm_name} ${fullname}
+
+# Build Singularity image from local docker image
+sifname="local_sagemaker-keras-text-classification_gpu.sif"
+sudo singularity build ${sifname} docker-daemon:${fullname}
+
+################################
+########## Local Test ########## 
+################################
+cd ../data
+cp -a . ../container/local_test/test_dir/input/data/training/
+cd ../container
+cd local_test
+
+### Train
+./train_local_gpu.sh ../${sifname}
+
+### Prediction
+#./serve_local.sh ../${fullname}
